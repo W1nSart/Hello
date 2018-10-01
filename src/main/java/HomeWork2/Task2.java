cacd4af5b0ce631d08acb828d61fa92bd9e6571c
mw260271gei/A-Bank24_IOS.git <git@git.corezoid.com>
2018-10-01 14:28:53 +0300

    some changes

--- src/main/java/HomeWork2/Task2.java
+++ src/main/java/HomeWork2/Task2.java
@@ -3,13 +3,13 @@ package HomeWork2;
 public class Task2 {
     public static void main(String[] args) {
 
-        long[] age = {1, 5, 8, 7, 5};
-        long max = age[0];
-        long min = age[0];
+        long[] numbers = {1, 5, 8, 7, 5};
+        long max = numbers[0];
+        long min = numbers[0];
 
-        for (int i = 1; i < age.length; i++) max = Math.max(max, age[i]);
-        for (int i = 1; i > age.length; i++) min = Math.max(min, age[i]);
+        for (int i = 1; i < numbers.length; i++) max = Math.max(max, numbers[i]);
+        for (int i = 1; i > numbers.length; i++) min = Math.max(min, numbers[i]);
         System.out.println(min);
         System.out.println(max);
 
-    }}
+    }}ush
