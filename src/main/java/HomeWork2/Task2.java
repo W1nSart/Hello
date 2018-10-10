b4f4bfa1f2827f67e735729074fdc9e810af0ecc
mw260271gei/A-Bank24_IOS.git <git@git.corezoid.com>
2018-10-10 18:31:56 +0300

    add minmax

--- src/main/java/HomeWork2/Task2.java
+++ src/main/java/HomeWork2/Task2.java
@@ -1,16 +1,35 @@
-
 package HomeWork2;
 
+import java.util.Scanner;
+
 public class Task2 {
+    public static long number;
+    public static long min = 9;
+    public static long max = 0;
+
+    public static void getNumber() {
+        System.out.println("Введите число");
+        number = new Scanner(System.in).nextLong();
+    }
+
+    public static void divide() {
+        while (number > 0) {
+            long current = number % 10;
+            if (current > max) {
+                max = current;
+            }
+            if (current < min) {
+                min = current;
+            }
+            number = number / 10;
+        }
+    }
+
     public static void main(String[] args) {
+        getNumber();
+        divide();
+        System.out.printf("Минимальное значение : %s%nМаксимальное значение : %s%n", min, max);
 
-        long[] numbers = {1, 5, 8, 7, 5};
-        long max = numbers[0];
-        long min = numbers[0];
 
-        for (int i = 1; i < numbers.length; i++) max = Math.max(max, numbers[i]);
-        for (int i = 1; i > numbers.length; i++) min = Math.max(min, numbers[i]);
-        System.out.println(min);
-        System.out.println(max);
-
-    }}
+    }
+}
